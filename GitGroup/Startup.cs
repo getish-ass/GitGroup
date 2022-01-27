62a5a8f517a7fabff84786d917815650007b8a9b
Getahun Assefa <getitpro@gmail.com>
2022-01-27 03:36:13 +0100

    Revert "First change in startup as requested in git"
    This reverts commit 9ae26208114c8bdfd83cff82b4ad55c0b15bda85.

--- GitGroup/Startup.cs
+++ GitGroup/Startup.cs
@@ -50,7 +50,7 @@ namespace GitGroup
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Privacy}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
